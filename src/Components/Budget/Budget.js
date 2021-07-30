93a7fd1cffdbf1c3426ef37e868ccc9e4023ac80
Anne Golladay <annegolladay@gmail.com>
2021-07-30 15:25:11 -0600

    saving work

--- src/Components/Budget/Budget.js
+++ src/Components/Budget/Budget.js
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Background from './../shared/Background/Background'
-import Chart1 from './../shared/Chart1';
-import Chart2 from './../shared/Chart2';
-import AddPurchase from './../shared/AddPurchase';
-import DisplayPurchases from './../shared/DisplayPurchases';
-import Loading from './../shared/Loading/Loading';
-import Nav from './../shared/Nav';
-import './Budget.css';
+import Chart1 from './../shared/Chart1'
+import Chart2 from './../shared/Chart2'
+import AddPurchase from './../shared/AddPurchase'
+import DisplayPurchases from './../shared/DisplayPurchases'
+import Loading from './../shared/Loading/Loading'
+import Nav from './../shared/Nav'
+import './Budget.css'
 import { connect } from 'react-redux'
+import { requestUserData } from '../../redux/userReducer'
+import { requestBudgetData, addPurchase, removePurchase } from '../../redux/budgetReducer'
 
 
 class Budget extends Component {
 
+  componentDidMount() {
+    this.props.requestUserData()
+    this.props.requestBudgetData()
+  }
+
   render() {
-    const { loading } = this.props.budget
+    const { loading, purchases, budgetLimit } = this.props.budget
+    const { firstName, lastName } = this.props.user
     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              {
+              
+              }
+              <AddPurchase addPurchase={this.props.addPurchase} />
+              <DisplayPurchases purchases={purchases} removePurchase={this.props.removePurchase} />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -35,12 +46,11 @@ class Budget extends Component {
   }
 }
 
-
 function mapStateToProps(state) {
   return {
-    budget: state.budget
+    budget: state.budget, 
+    user: state.user
   }
 }
 
-
-export default connect(mapStateToProps)(Budget)
+export default connect(mapStateToProps, { requestUserData, requestBudgetData, addPurchase, removePurchase })(Budget)
